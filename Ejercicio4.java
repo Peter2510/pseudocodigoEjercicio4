9b146ba87ae8b2c544e76c75199723bc75dbc934
Pedro Gordillo <pedrogordillo2510@gmail.com>
2021-06-09 23:57:04 -0600

    Ingresar 20 edades y calcular la mayor y la menor

---
+++ Ejercicio4.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+
+public class Ejercicio4{
+
+public static void main(String [] args){
+
+	// variables
+	int edad;
+
+	int mayor =0;
+	int menor =0;
+
+	//definir el scanner
+
+	Scanner num = new Scanner(System.in);
+
+	//Solicitar las edades
+
+
+	for (int i=0; i<20 ;i++ ) {
+
+
+	System.out.print("\nIngrese un numero: ");
+	edad = num.nextInt();
+
+	//salto de linea
+	System.out.print("\n");
+
+		if (i==0) {
+
+			mayor = edad;
+			menor = edad;
+			System.out.println("el mayor es"+ mayor+ "menor"+menor);
+			
+		}
+
+		else{
+
+			if (edad > mayor) {
+
+				mayor = edad;
+
+				System.out.println("entre11");
+				System.out.println("el mayor es"+ mayor+ "menor"+menor);
+
+
+			}
+
+			if (edad < menor) {
+
+				menor = edad;
+
+				System.out.println("entre11");
+				System.out.println("el mayor es"+ mayor+ "menor"+menor);
+
+
+		}
+
+
+		
+	}
+
+
+  }
+
+}
